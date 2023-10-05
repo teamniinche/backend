0c368c1eb0313cfe86b5e6b2a5cdb582cbb3f8e2
Mamadou NDOUR <ndourm9@gmail.com>
2023-10-05 21:21:49 +0000

    Correction de lerreur sur lajout de la politique CORS 19

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const app=express();
 // Set middleware of CORS 
 app.use(cors())
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin","https://tnserver.onrender.com");
+  res.header("Access-Control-Allow-Origin","*");
   res.header("Access-Control-Allow-Methods","GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE");
   res.header("Access-Control-Allow-Headers","Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
