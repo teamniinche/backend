f6cf8245ec8e1bb2cbefbbbc1f6b0e6837b22ffe
Mamadou NDOUR <ndourm9@gmail.com>
2023-10-05 17:05:31 +0000

    Correction de lerreur sur lajout de la politique CORS 3

--- server.js
+++ server.js
@@ -25,17 +25,9 @@ const bodyParser = require('body-parser');
 const app=express();
 // Set middleware of CORS 
 app.use((req, res, next) => {
-  req.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://www.teamniintcheft.onrender.com"
-  );
-  req.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  res.setHeader("Access-Control-Allow-Origin","https://www.teamniintcheft.onrender.com ");
+  res.setHeader("Access-Control-Allow-Methods","GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE");
+  res.setHeader("Access-Control-Allow-Headers","Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Private-Network", true);
