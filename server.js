6848443932e0359575ee3cccf09fde658afe92a5
Mamadou NDOUR <ndourm9@gmail.com>
2023-10-06 08:26:17 +0000

    Ajout de la mise jour de server.js

--- server.js
+++ server.js
@@ -28,13 +28,13 @@ const app=express();
 app.use(cors())
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin","*");
-  res.header("Access-Control-Allow-Methods","GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE");
-  res.header("Access-Control-Allow-Headers","Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.header("Access-Control-Max-Age", 7200);
+  res.header("Access-Control-Allow-Methods","GET,PUT,POST,DELETE");//"GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE");
+  // res.header("Access-Control-Allow-Headers","Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  // );
+  // res.header("Access-Control-Allow-Credentials", true);
+  // res.header("Access-Control-Allow-Private-Network", true);
+  // //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  // res.header("Access-Control-Max-Age", 7200);
 
   next();
 });
