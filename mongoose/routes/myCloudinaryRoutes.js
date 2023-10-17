77c84c41e3dcd126e4cda44347d9ad5dc95be49f
Mamadou NDOUR <ndourm9@gmail.com>
2023-10-17 21:14:51 +0000

    Essai de l'image par defaut

--- mongoose/routes/myCloudinaryRoutes.js
+++ mongoose/routes/myCloudinaryRoutes.js
@@ -13,7 +13,7 @@ const signuploadform = (filename) => {
   const signature = cloudinary.utils.api_sign_request({
     timestamp: timestamp,
     eager: 'c_pad,h_200,w_200|c_crop,h_200,w_200',
-    public_id:"logo_niintche_blanc.ico",//filename,
+    public_id:filename,
     folder: 'signed_upload_demo_form/membres'}, apiSecret);
 
   return { timestamp, signature }
