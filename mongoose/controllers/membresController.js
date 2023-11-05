a6f37fa1fc0866161f506f926ba0509bd5458b13
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2023-11-05 03:37:19 +0000

    Update membresController.js

--- mongoose/controllers/membresController.js
+++ mongoose/controllers/membresController.js
@@ -136,16 +136,7 @@ module.exports.maj = async (req, res) => {
 module.exports.changePassWord = async (req, res) => {
     const membrePseudo = req.params.pseudo;
     const {Ancien,Nouveau,Confirmation}=req.body
-    const isValid=await new Promise((resolve,reject)=>{
-        const membre= await membres.filter(membre=>membre.pseudo===membrePseudo)
-        if(membre.length===0){reject(err)}
-        else{
-            bcrypt.compare(Ancien,membre[0].passWord,function(err,isValid){
-            if(err) reject(err)
-            resolve(isValid)
-            });
-        }
-    })
+
     const deggat= await new Promise((resolve,reject)=>{
         const odiem=bcrypt.genSaltSync(10)
         bcrypt.hash(Nouveau,odiem,function(err,hash){
@@ -154,6 +145,16 @@ module.exports.changePassWord = async (req, res) => {
         });
     })
     try {
+        const isValid=await new Promise((resolve,reject)=>{
+        const membre= membres.filter(membre=>membre.pseudo===membrePseudo)
+        if(membre.length===0){reject(err)}
+        else{
+            bcrypt.compare(Ancien,membre[0].passWord,function(err,isValid){
+            if(err) reject(err)
+            resolve(isValid)
+            });
+        }
+    })
         if(!isValid){
             return res.status(400).send({'retour':'Mot de passe non reconnu ❕'})}
         else{ 
