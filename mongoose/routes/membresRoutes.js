2b78a0b0ab7449a1b8d0093f68d0d51effb851dc
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2023-11-09 20:42:50 +0000

    Update membresRoutes.js

--- mongoose/routes/membresRoutes.js
+++ mongoose/routes/membresRoutes.js
@@ -13,7 +13,7 @@ router.delete(':id',membresController.delete)
 // Updating membre
 router.put('/:pseudo',membresController.maj)
 router.put('/user/:pseudo',membresController.changePassWord)
-router.put('/from/galerie/add/updateImages',membresController.addIIRemoveImage)
+router.put('/from/galerie/add/:pseudo',membresController.addIIRemoveImage)
 router.put('/galerie/:pseudo/:sProp',membresController.majGalerie)
 router.put('/admin/:pseudo',membresController.majProps)
 router.put('/userRs/:pseudo',membresController.majRs)
