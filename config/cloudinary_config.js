9426e3a20ed096defc21f6475aa2c31c3c142cf2
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2024-06-20 12:58:49 +0000

    Update cloudinary_config.js

--- config/cloudinary_config.js
+++ config/cloudinary_config.js
@@ -3,9 +3,9 @@ const cloudinary = require('cloudinary').v2;
 // Configure your cloud name, API key and API secret:
 
 const myconfig = cloudinary.config({
-  cloud_name: 'dapkl1ien',
-  api_key: '327969554979561',
-  api_secret: 'w0rvj0T50Jf6HHAjx1B8_oo06es',
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
   secure: true
 });
 
