1ba341f86c7a1a0f0c0a955794b6924398dd2177
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2024-06-29 16:52:51 +0000

    Update membresController.js

--- mongoose/controllers/membresController.js
+++ mongoose/controllers/membresController.js
@@ -332,7 +332,7 @@ module.exports.confirmEmail=async (req,res)=>{
             port: 465,
             secure: true,
             auth: {
-              user: 'ndourm9@gmail.com',
+              user: 'teamniintche@gmail.com',
               pass: process.env.NODEMAILER_AUTH_PASS,
             },   
         }
